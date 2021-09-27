53988657e13b9bcba071cddc65d2f0cb43cacde5
Elizaveta <eli3avetta@mail.ru>
2021-09-27 12:11:37 +0300

    swiper 3

--- src/js/common.js
+++ src/js/common.js
@@ -1,10 +1,7 @@
 import polyfills from './finctions/polyfills';
 import detectTouch from './finctions/detectTouch';
-import initSlider from './finctions/initSlider';
 
 export default function common() {
   polyfills();
   detectTouch();
-
-  initSlider(4);
 }
--- src/js/pages/main-page.js
+++ src/js/pages/main-page.js
@@ -1,7 +1,11 @@
 import common from '../common';
+import initSlider from '../finctions/initSlider';
 
 document.addEventListener('DOMContentLoaded', () => {
   common();
+
+  initSlider(4);
+  initSlider(3);
 });
 
 window.addEventListener('load', function () {
