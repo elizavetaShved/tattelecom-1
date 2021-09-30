4b2372e8c3a6a9cc5fbce9fd6ed5729fb5d376e7
Elizaveta <eli3avetta@mail.ru>
2021-09-30 16:29:47 +0300

    fix

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -7,11 +7,19 @@ export class Header {
     const popupElem = hostElem.querySelector('.header__popup-container');
     const popupWrapperElem = hostElem.querySelector('.header__popup-wrapper');
     const bottomBlockElem = hostElem.querySelector('.header__bottom-block-content');
-    const menuLinkElements = hostElem.querySelectorAll('.header__bottom-menu-link');
+    const menuBtnElements = hostElem.querySelectorAll('.header__bottom-menu-btn');
+    const popupContentElements = Array.from(hostElem.querySelectorAll('.header__popup-content'));
 
-    menuLinkElements.forEach(link => {
-      link.onmouseover = () => {
+    menuBtnElements.forEach(btn => {
+      btn.onmouseover = () => {
         popupElem.classList.add('mod-show');
+
+        popupContentElements.forEach(popupContentElem => {
+          if (popupContentElem.className.includes(`mod-${ btn.value }`) && !popupContentElem.className.includes('mod-show')) {
+            popupContentElements.map(elem => elem.classList.remove('mod-show'));
+            popupContentElem.classList.add('mod-show');
+          }
+        })
       }
     })
 
