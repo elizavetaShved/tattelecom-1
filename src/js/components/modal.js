e36d00c3f7eee5de375b54dc47c4b9a612f80c1b
Elizaveta <eli3avetta@mail.ru>
2021-10-04 11:09:44 +0300

    modal class

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -2,14 +2,28 @@ import { checkExistParent } from '../finctions/checkExistParent';
 
 export class Modal {
   modalElem;
+  modalContainerElem;
   isOpenModal = false;
+  isOpenThanksModal = false;
 
-  constructor(btnOpenModalCSSSelector, btnCloseModalCSSSelector, modalClassCSSSelector) {
-    this.checkClickByModalCallback = this.checkClickByModalCallback.bind(this);
+  modalThanksElem;
+  modalThanksContainerElem;
+  modalThanksCloseBtn;
+
+  constructor(btnOpenModalCSSSelector, modalClassCSSSelector) {
+    this.checkClickByModal = this.checkClickByModal.bind(this);
+    this.checkClickByModalThanks = this.checkClickByModalThanks.bind(this);
     const btnsOpen = document.querySelectorAll(btnOpenModalCSSSelector);
     if (btnsOpen) {
       this.modalElem = document.querySelector(modalClassCSSSelector);
-      const btnsClose = document.querySelectorAll(btnCloseModalCSSSelector);
+      this.modalContainerElem = this.modalElem.querySelector('.js-modal-container');
+      const btnSubmitForm = this.modalElem.querySelector('.js-btn-submit');
+      const formModal = this.modalElem.querySelector('.js-form-modal');
+      const btnsClose = this.modalElem.querySelectorAll('.js-close-modal');
+
+      this.modalThanksElem = document.querySelector('#modal-thanks');
+      this.modalThanksContainerElem = document.querySelector('#modal-thanks-container');
+      this.modalThanksCloseBtn = document.querySelector('#close-thanks-modal');
 
       btnsOpen.forEach(btn => {
         btn.onclick = () => {
@@ -22,24 +36,53 @@ export class Modal {
           this.onCloseModal();
         }
       })
+
+      this.modalThanksCloseBtn.onclick = () => {
+        this.onCloseModalThanks();
+      }
+
+      // todo убрать с фронта
+      btnSubmitForm.onclick = () => {
+        if ($(formModal).parsley().isValid()) {
+          this.onCloseModal();
+          this.modalThanksElem.classList.add('mod-show');
+          document.addEventListener('click', this.checkClickByModalThanks);
+        }
+      }
     }
+
+    document.addEventListener('click', this.checkClickByModal);
   }
 
   onOpenModal() {
     this.modalElem.classList.add('mod-show');
-    document.addEventListener('click', this.checkClickByModalCallback);
   }
 
   onCloseModal() {
     this.modalElem.classList.remove('mod-show');
-    document.removeEventListener('click', this.checkClickByModalCallback);
+    this.isOpenModal = false;
+    document.removeEventListener('click', this.checkClickByModal);
   }
 
-  checkClickByModalCallback(event) {
-    this.isOpenModal = false;
-    this.isOpenModal = !checkExistParent(event.target, this.modalElem);
-    if (!this.isOpenModal) {
+  checkClickByModal(event) {
+    if (this.isOpenModal && !checkExistParent(event.target, this.modalContainerElem)) {
       this.onCloseModal();
+    } else {
+      this.isOpenModal = true;
     }
+  }
+
+  checkClickByModalThanks(event) {
+    if (this.isOpenThanksModal && !checkExistParent(event.target, this.modalThanksContainerElem)) {
+      this.onCloseModalThanks();
+    } else {
+      this.isOpenThanksModal = true;
+    }
+  }
+
+  onCloseModalThanks() {
+    this.modalThanksElem.classList.remove('mod-show');
+    this.isOpenThanksModal = false;
+    document.removeEventListener('click', this.checkClickByModalThanks);
   }
 }
--- src/js/finctions/initModal.js
+++ src/js/finctions/initModal.js
@@ -1,12 +1,17 @@
 // модалка "Оставьте свой номер, мы перезвоним Вам в течение 15 минут."
 // __классы для кнопок__
+// .js-open-callback-modal - на кнопки открывания модалки
+// .js-close-modal - на кнопку закрывания модалки
+// #modal-callback-host - компонент модалки
+// .js-modal-container - окно модалки
+// .js-form-modal - форма в модалке
+// .js-btn-submit - кнопка сабмита в форме додалки
 
 import { Modal } from '../components/modal';
 
 export default function initModal() {
   new Modal(
     '.js-open-callback-modal',
-    '.js-close-callback-modal',
     '#modal-callback-host'
   );
 }
