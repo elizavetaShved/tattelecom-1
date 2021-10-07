09cbfd1ba7e0497af37d77ea4d1ebd0aaf79a81a
Elizaveta <eli3avetta@mail.ru>
2021-10-08 01:05:17 +0300

    modal

--- src/js/finctions/initModals/initSendRequestModal.js
+++ src/js/finctions/initModals/initSendRequestModal.js
@@ -13,9 +13,15 @@ export default function initSendRequestModal() {
   const btnsOpen = document.querySelectorAll('.js-open-send-request-modal');
   const btnsClose = document.querySelectorAll('.js-close-send-request-modal');
   const modalElem = document.querySelector('#modal-send-request-host');
-  const modalTitleElem = modalElem.querySelector('.modal__title');
+  const dataTitleElem = modalElem.querySelector('.modal__title');
   const modalPriceWrapperElem = modalElem.querySelector('.send-request__price');
   const modalPriceElem = modalElem.querySelector('.connect-tariff__price .number');
+
+  const modalProductWrapperElem = modalElem.querySelector('.send-request__product-wrapper');
+  const modalProductImgElem = modalProductWrapperElem.querySelector('.send-request__product-img');
+  const modalProductCaptionElem = modalProductWrapperElem.querySelector('.send-request__product-caption');
+  const modalProductValueElem = modalProductWrapperElem.querySelector('.send-request__product-value');
+  const modalProductPriceElem = modalProductWrapperElem.querySelector('.send-request__product-price');
 
   let modalComponent;
 
@@ -24,13 +30,45 @@ export default function initSendRequestModal() {
       modalComponent = new Modal(modalElem);
       modalComponent.onOpenModal();
 
-      const modalTitle = btn.getAttribute('data-tariff-name');
-      const modalPrise = btn.getAttribute('data-prise');
-      modalTitleElem.innerText = modalTitle;
-      if (modalPrise) {
-        modalPriceElem.innerText = modalPrise;
+      const dataTitle = btn.getAttribute('data-tariff-name');
+      const dataPrise = btn.getAttribute('data-prise');
+
+      const dataProductImg = btn.getAttribute('data-product-img');
+      const dataProductCaption = btn.getAttribute('data-product-caption');
+      const dataProductValue = btn.getAttribute('data-product-value');
+      const dataProductPrice = btn.getAttribute('data-product-price');
+      const dataPointIssue = btn.hasAttribute('data-point-issue');
+
+      dataTitleElem.innerText = dataTitle;
+      if (dataPrise) {
+        modalPriceElem.innerText = dataPrise;
       } else {
         modalPriceWrapperElem.remove();
+      }
+
+
+      if (dataProductImg || dataProductCaption || dataProductValue || modalProductPrice) {
+        modalProductWrapperElem.classList.add('mod-show');
+
+        if (dataProductImg) {
+          modalProductImgElem.setAttribute('src', dataProductImg);
+        }
+
+        if (dataProductCaption) {
+          modalProductCaptionElem.innerText = dataProductCaption;
+        }
+
+        if (dataProductValue) {
+          modalProductValueElem.innerText = dataProductValue;
+        }
+
+        if (dataProductPrice) {
+          modalProductPriceElem.innerText = dataProductPrice;
+        }
+      }
+
+      if (dataPointIssue) {
+
       }
     });
   })
--- src/js/pages/product-page.js
+++ src/js/pages/product-page.js
@@ -1,8 +1,12 @@
 import common from '../common';
 import initSlider from '../finctions/initSlider';
+import initSendRequestModal from '../finctions/initModals/initSendRequestModal';
+import initSelects from '../finctions/initSelects';
 
 document.addEventListener('DOMContentLoaded', () => {
   common();
 
-  initSlider(4)
+  initSlider(4);
+  initSendRequestModal();
+  initSelects();
 });
