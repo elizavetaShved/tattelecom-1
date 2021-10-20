3f418bd3919c73df6d6b706c79c03f70fa909a66
Elizaveta <eli3avetta@mail.ru>
2021-10-20 12:00:01 +0300

    карта

---
+++ src/js/components/map.js
@@ -0,0 +1,43 @@
+export class Map {
+  hostElem;
+  mapElem;
+  marksList;
+  myMap;
+
+  constructor(hostElem) {
+    this.initMap = this.initMap.bind(this);
+
+    this.hostElem = hostElem;
+    this.mapElem = this.hostElem.querySelector('.js-map');
+
+    ymaps.ready(this.initMap)
+  }
+
+  initMap() {
+    const center = JSON.parse(this.mapElem.getAttribute('data-center'));
+
+    this.myMap = new ymaps.Map(this.mapElem, {
+      center: center,
+      zoom: 10
+    }, {
+      searchControlProvider: 'yandex#search'
+    });
+
+    this.myMap.behaviors.disable('scrollZoom');
+
+    this.marks = JSON.parse(this.mapElem.getAttribute('data-mark-coords'));
+
+    this.marks.forEach(mark => {
+      this.addGeoMark(mark[0], mark[1], mark[2]);
+    })
+  }
+
+  addGeoMark(x, y, name) {
+    const myPlacemark = new ymaps.Placemark([x, y], {
+      iconCaption: name
+    }, {
+      preset: 'islands#darkOrangeIcon'
+    });
+    this.myMap.geoObjects.add(myPlacemark);
+  }
+}
--- src/js/pages/promo-gpon-page.js
+++ src/js/pages/promo-gpon-page.js
@@ -2,11 +2,16 @@ import common from '../common';
 import initSlider from '../finctions/initSlider';
 import validation from '../finctions/validation';
 import phoneMask from '../finctions/phoneMask';
+import { Map } from '../components/map';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const hostElem = document.querySelector('#gpon-map-host');
+
   common();
 
   initSlider(4);
   validation();
   phoneMask();
+
+  new Map(hostElem);
 });
