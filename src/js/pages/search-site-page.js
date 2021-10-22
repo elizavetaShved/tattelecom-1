89db8edabd0c784bdd14f7d8472b1b734c2ed1f4
Elizaveta <eli3avetta@mail.ru>
2021-10-22 09:44:24 +0300

    new page

---
+++ src/js/pages/search-site-page.js
@@ -0,0 +1,8 @@
+import common from '../common';
+import initSelects from '../finctions/initSelects';
+
+document.addEventListener('DOMContentLoaded', () => {
+  common();
+
+  initSelects(4)
+});
--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ module.exports = {
     ['promo-convergent-page']: './src/js/pages/promo-convergent-page.js',
     ['error-page']: './src/js/pages/error-page.js',
     ['payment-services-page']: './src/js/pages/payment-services-page.js',
+    ['search-site-page']: './src/js/pages/search-site-page-page.js',
   },
   output: {
     path: path.resolve(__dirname, './build/js'),
