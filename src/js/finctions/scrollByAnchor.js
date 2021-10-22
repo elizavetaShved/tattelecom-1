7ba81ca9a65abc61bf197e7b1d40f566539ad861
MelentQ <mejleht@yandex.ru>
2021-10-22 19:05:41 +0300

    Страница "Часто задаваемые вопросы"

--- src/js/finctions/scrollByAnchor.js
+++ src/js/finctions/scrollByAnchor.js
@@ -1,7 +1,11 @@
-export default function scrollByAnchor() {
-  $("a[href^='#']").click(function () {
-    const _href = $(this).attr("href");
-    $("html, body").animate({ scrollTop: $(_href).offset().top + 'px' });
-    return false;
-  });
+/**
+ * Плавный сколл для якорных ссылок
+ * @param {Number} topIndent отступ сверху
+ */
+export default function scrollByAnchor(topIndent = 0) {
+    $("a[href^='#']").click(function() {
+        const _href = $(this).attr('href');
+        $('html, body').animate({ scrollTop: $(_href).offset().top - topIndent + 'px' });
+        return false;
+    });
 }
