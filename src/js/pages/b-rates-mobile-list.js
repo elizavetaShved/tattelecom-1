987b361967329df57e058d279953a31d8ef94ff7
MelentQ <mejleht@yandex.ru>
2021-10-28 14:19:48 +0300

    Страница "Тарифы мобильной связи"

--- src/js/pages/b-rates-mobile-list.js
+++ src/js/pages/b-rates-mobile-list.js
@@ -1,11 +1,12 @@
-import alignHeights from "../finctions/alignHeights";
 import initSlider from "../finctions/initSlider";
+import validation from '../finctions/validation';
+import phoneMask from '../finctions/masks/phoneMask';
+import initDropDownMenu from "../finctions/initDropDownMenu";
 
 document.addEventListener('DOMContentLoaded', () => {
   initSlider(3);
   initSlider(4);
+  validation();
+  phoneMask();
+  initDropDownMenu();
 });
-
-window.addEventListener('load', () => {
-  alignHeights('#js-align-tariffs-heights', '.b-tariffs-item__services-list');
-})
