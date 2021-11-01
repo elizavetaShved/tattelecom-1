e116e834658e7bcef4e9698af9769ab5701f4986
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-11-01 13:28:38 +0300

    main page

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -8,6 +8,25 @@ export class Slider {
 
     const gap = customGap ? +customGap : 30;
 
+    let slidesPerView769;
+    let slidesPerView1025;
+
+    switch (true) {
+      case numSlide === 'auto':
+        slidesPerView769 = numSlide;
+        slidesPerView1025 = numSlide;
+        break;
+
+      case  numSlide > 2:
+        slidesPerView769 = numSlide - 1 + 0.2;
+        slidesPerView1025 = numSlide - 1;
+        break;
+
+      default:
+        slidesPerView769 = numSlide + 0.2;
+        slidesPerView1025 = numSlide;
+      }
+
     new Swiper(sliderElem, {
       slidesPerView: 1.2,
       lazy: true,
@@ -23,11 +42,11 @@ export class Slider {
       },
       breakpoints: {
         769: {
-          slidesPerView: numSlide > 2 ? numSlide - 1 + 0.2: numSlide + 0.2,
+          slidesPerView: slidesPerView769,
           spaceBetween: gap - 10 > 16 ? gap - 10 : 16
         },
         1025: {
-          slidesPerView: numSlide > 2 ? numSlide - 1 : numSlide,
+          slidesPerView: slidesPerView1025,
         },
         1201: {
           slidesPerView: numSlide,
