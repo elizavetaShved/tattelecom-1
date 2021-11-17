8c3b0bb15eea725123d00b81de83dce488d37590
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-11-17 12:38:01 +0300

    fix link js

--- webpack.config.js
+++ webpack.config.js
@@ -50,7 +50,7 @@ module.exports = {
     ['b-internet-by-radio-page']: './src/js/pages/b-internet-by-radio-page',
     ['b-virtual-networks-page']: './src/js/pages/b-virtual-networks-page',
     ['b-lorawan-page']: './src/js/pages/b-lorawan-page',
-    ['b-m2m-page']: './src/js/pages/b-m2m-page-page',
+    ['b-m2m-page']: './src/js/pages/b-m2m-page',
     ['b-colocation-page']: './src/js/pages/b-colocation-page',
     ['b-virtual-server-page']: './src/js/pages/b-virtual-server-page',
     ['b-web-hosting-page']: './src/js/pages/b-web-hosting-page',
