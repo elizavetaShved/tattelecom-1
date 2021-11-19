882d0bdd8e1f84894264927e81e6b7d84b6b8ad9
elizavetaShved <eli3avetta.shvedchikova@gmail.com>
2021-11-19 17:28:50 +0300

    Интернет для офиса

--- src/js/finctions/initSlider.js
+++ src/js/finctions/initSlider.js
@@ -1,10 +1,11 @@
 import { Slider } from '../components/slider';
 
-// 1ур. gl-slider + js-swiper data-swiper-slides-preview="4" + data-swiper-gap="20"? - общая обертка:
-// 2ур. gl-slider__container + swiper-container + js-swiper-container
-// 3ур. gl-slider__wrapper + swiper-wrapper
-// 4ур. swiper-slide
+// 1ур. .gl-slider.js-swiper data-swiper-slides-preview="4" + data-swiper-gap="20"? - общая обертка:
+// 2ур. .gl-slider__container.swiper-container.js-swiper-container
+// 3ур. .gl-slider__wrapper.swiper-wrapper
+// 4ур. .swiper-slide
 // 2ур. {{> common/controls/btns-slider }} - кнопки
+// 2ур. {{> common/controls/b-btns-slider }} - кнопки -2
 
 export default function initSlider() {
   const sliderWrapperElements = Array.from(document.querySelectorAll(`.js-swiper`));
