6804c367161dfd14308174f5677da0fc717b1e47
Elizaveta <eli3avetta@mail.ru>
2021-11-26 16:18:01 +0300

    fixes

--- src/js/finctions/setMaxHeightElem.js
+++ src/js/finctions/setMaxHeightElem.js
@@ -2,6 +2,7 @@ export default function setMaxHeightElem(elementsArr) {
   const setHeight = () => {
     let maxHeight = 0;
     elementsArr.forEach(elem => {
+      elem.style.height = 'auto';
       maxHeight = elem.clientHeight > maxHeight ? elem.clientHeight : maxHeight;
     })
 
