8ddfdae8045444cfda33f2bbab677a80097ea502
Elizaveta <eli3avetta@mail.ru>
2021-11-26 14:12:32 +0300

    fix

--- src/js/pages/b-communication-page.js
+++ src/js/pages/b-communication-page.js
@@ -1,7 +1,9 @@
 import initSlider from '../finctions/initSlider';
 import phoneMask from '../finctions/masks/phoneMask';
+import initMenuTabs from '../finctions/initMenuTabs';
 
 document.addEventListener('DOMContentLoaded', () => {
   initSlider();
   phoneMask();
+  initMenuTabs();
 });
