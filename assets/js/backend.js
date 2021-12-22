2063a78c85c07a2f20184cdb8dd457a342d3ce6e
Nail <nailgalimov8@gmail.com>
2021-12-22 19:03:27 +0300

    Updates

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -166,4 +166,58 @@ document.addEventListener('DOMContentLoaded', () => {
       form.submit();
     });
   }
+
+  // табы инициализация
+  function initDropDownMenu() {
+    const linksTitleElements = Array.from(document.querySelectorAll('.js-drop-menu-btn'));
+    const menuContainerElements = Array.from(document.querySelectorAll('.js-drop-menu-container'));
+    const menuElements = Array.from(document.querySelectorAll('.js-drop-menu'));
+  
+    const onClose = (btnElem, menuContainerElem, contentBtnHide, contentBtnOpen) => {
+      btnElem.classList.remove('mod-open');
+      menuContainerElem.classList.remove('mod-open');
+      menuContainerElem.style.maxHeight = 0;
+  
+      if (contentBtnHide && contentBtnOpen) {
+        contentBtnHide.classList.add('mod-show');
+        contentBtnOpen.classList.remove('mod-show');
+      }
+    }
+  
+    const onOpen = (btnElem, menuContainerElem, menuElem, contentBtnHide, contentBtnOpen) => {
+      btnElem.classList.add('mod-open');
+      menuContainerElem.classList.add('mod-open');
+      const heightContent = menuElem.clientHeight;
+      menuContainerElem.style.maxHeight = `${ heightContent }px`;
+  
+      if (contentBtnHide && contentBtnOpen) {
+        contentBtnHide.classList.remove('mod-show');
+        contentBtnOpen.classList.add('mod-show');
+      }
+    }
+  
+    linksTitleElements.forEach((btn, i) => {
+      if (btn.hasAttribute('data-drop-menu-open')) {
+        onOpen(btn, menuContainerElements[i], menuElements[i]);
+      }
+  
+      const contentBtnHide = btn.querySelector('.js-drop-menu-btn-content-hide');
+      const contentBtnOpen = btn.querySelector('.js-drop-menu-btn-content-open');
+  
+      btn.onclick = () => {
+        if (btn.className.includes('mod-open')) {
+          onClose(btn, menuContainerElements[i], contentBtnHide, contentBtnOpen);
+        } else {
+          if (!btn.className.includes('js-drop-menu-leave-open')) {
+            linksTitleElements.map((elem, idx) => {
+              onClose(elem, menuContainerElements[idx], contentBtnHide, contentBtnOpen);
+            });
+          }
+          onOpen(btn, menuContainerElements[i], menuElements[i], contentBtnHide, contentBtnOpen);
+        }
+      }
+    })
+  }
+
+  window.initDropDownMenu = initDropDownMenu;
 });
