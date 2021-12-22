9be37bd641b409ec6255d7944b384a38d867af4e
Nail <nailgalimov8@gmail.com>
2021-12-22 12:32:24 +0300

    add autoplay index hero slider amd some styles on mobile img

--- src/js/finctions/initHeroSlider.js
+++ src/js/finctions/initHeroSlider.js
@@ -1,9 +1,14 @@
-import { Navigation, Swiper, Pagination } from 'swiper';
+import { Navigation, Swiper, Pagination, Autoplay } from 'swiper';
 
-Swiper.use([Navigation, Pagination]);
+Swiper.use([Navigation, Pagination, Autoplay]);
 
 export default () => {
+    const autoplayValue = document.querySelector(".js-swiper-custom").getAttribute('data-swiper-autoplay')
     let sliderHero = new Swiper(".js-swiper-custom", {
+        speed: 400,
+        autoplay: {
+            delay: +autoplayValue,
+        },
         pagination: {
 			el: '.rates-preview__sldier-pagination',
             clickable: true,
